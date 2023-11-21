1ad56c0f0800e9c7861e21187e5ef7cf1994dfe2
Eemeli Haakana <eemeli.haakana@aalto.fi>
2023-11-21 09:31:56 +0200

    pixel

--- app/layout.js
+++ app/layout.js
@@ -30,8 +30,10 @@ export default function RootLayout({ children }) {
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'G-KH0RH1M52E');`}</Script>
-      <Script id="meta-pixel">{`<!-- Meta Pixel Code -->
-<script>
+      <Script
+        id="meta-pixel"
+        dangerouslySetInnerHTML={{
+          __html: `
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -42,11 +44,13 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '367998918902024');
 fbq('track', 'PageView');
-</script>
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=367998918902024&ev=PageView&noscript=1"
 /></noscript>
-<!-- End Meta Pixel Code -->`}</Script>
+<!-- End Meta Pixel Code -->
+`,
+        }}
+      />
       <body>
         <main>
           <LoaderPlaceholder />
