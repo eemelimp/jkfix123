9678942b37ed9e08d596a9edc43ce78c24465654
Eemeli Haakana <eemeli.haakana@aalto.fi>
2023-11-21 09:33:54 +0200

    pixel1

--- app/layout.js
+++ app/layout.js
@@ -44,9 +44,6 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '367998918902024');
 fbq('track', 'PageView');
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=367998918902024&ev=PageView&noscript=1"
-/></noscript>
 <!-- End Meta Pixel Code -->
 `,
         }}
