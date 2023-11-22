1b338fb4bbde3bbbbf10c7b0dbb98cf567965851
Eemeli Haakana <eemeli.haakana@aalto.fi>
2023-11-22 19:00:34 +0200

    gtm

--- app/layout.js
+++ app/layout.js
@@ -28,14 +28,15 @@ const jura = Jura({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={jura.className}>
-      <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-      >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KJVCZSNF');`}</Script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KJVCZSNF');
+        `}
+      </Script>
       {/* <Script id="google-analytics">{`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
@@ -63,12 +64,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         fbq('track', 'PageView');`}</Script> */}
 
       <body>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJVCZSNF"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        />
         {/* <Suspense>
           <GtmComponent />
         </Suspense> */}
@@ -83,6 +78,11 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <main>
         {children}
         </main> */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJVCZSNF" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   );
