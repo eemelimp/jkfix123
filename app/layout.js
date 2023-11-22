4eb8d3841d97549d31cc5831a83284a596b0fd8b
Eemeli Haakana <eemeli.haakana@aalto.fi>
2023-11-22 18:47:03 +0200

    GTM HTML

--- app/layout.js
+++ app/layout.js
@@ -53,7 +53,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '367998918902024');
         fbq('track', 'PageView');`}</Script> */}
+      <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KJVCZSNF');`}</Script>
       <body>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJVCZSNF"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         {/* <Suspense>
           <GtmComponent />
         </Suspense> */}
