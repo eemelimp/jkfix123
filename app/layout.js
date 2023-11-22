152e084d1d419be6fd2612fd2dfb8a50dd09ea4b
ehaakana <123645991+ehaakana@users.noreply.github.com>
2023-11-22 17:09:09 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -26,11 +26,12 @@ const jura = Jura({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={jura.className}>
-      <Script id="google-analytics">{`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-KH0RH1M52E');`}</Script>
-      <Script id="meta-pixel">{`!function(f,b,e,v,n,t,s)
+      <Script>{`<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KJVCZSNF');</script>`}</Script>
+      {/* <Script id="meta-pixel">{`!function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
         if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -39,8 +40,10 @@ export default function RootLayout({ children }) {
         s.parentNode.insertBefore(t,s)}(window, document,'script',
         'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '367998918902024');
-        fbq('track', 'PageView');`}</Script>
+        fbq('track', 'PageView');`}</Script> */}
       <body>
+        <Script>{`<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJVCZSNF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`}</Script>
         <main>
           <LoaderPlaceholder />
         </main>
