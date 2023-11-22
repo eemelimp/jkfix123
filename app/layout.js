ce2bb18ef48cf5974de073a7f215f2354ee6e538
Eemeli Haakana <eemeli.haakana@aalto.fi>
2023-11-22 18:08:20 +0200

    GTM

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import Header from "./components/Header/Header";
 import { Jura } from "next/font/google";
 import LoaderPlaceholder from "./LoaderPlaceholder";
 import Script from "next/script";
+import { Suspense } from "react";
+import GtmComponent from "./components/Analytics/GtmComponent";
 
 export const metadata = {
   title: {
@@ -31,11 +33,11 @@ export default function RootLayout({ children }) {
   gtag('js', new Date());
   gtag('config', 'G-KH0RH1M52E');`}</Script> */}
 
-      <Script>{`<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      {/* <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KJVCZSNF');</script>`}</Script>
+})(window,document,'script','dataLayer','GTM-KJVCZSNF');`}</Script> */}
       {/* <Script id="meta-pixel">{`!function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -47,8 +49,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         fbq('init', '367998918902024');
         fbq('track', 'PageView');`}</Script> */}
       <body>
-        <Script>{`<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJVCZSNF"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`}</Script>
+        <Suspense>
+          <GtmComponent />
+        </Suspense>
+        {/* <Script>{`<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJVCZSNF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`}</Script> */}
         <main>
           <LoaderPlaceholder />
         </main>
